34f82e05d3a5ed0682919d6227da919495486707
Marcin Krolik <krolikmarcin.mk@gmail.com>
2018-11-06 23:09:00 +0100

    Add gem "draper", decorate Book class and and pagination to controllers/books#index

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,9 +1,9 @@
 class BooksController < ApplicationController
   def index
-    if params[:query]
-      @books = Book.search_for(params[:query])
-    else
-      @books = Book.all
-    end
+    @books = if params[:query]
+               Book.search_for(params[:query]).page(params[:page]).decorate
+             else
+               Book.all.page(params[:page]).decorate
+             end
   end
 end
---
+++ app/decorators/book_decorator.rb
@@ -0,0 +1,37 @@
+class BookDecorator < ApplicationDecorator
+  delegate_all
+
+  def number(page)
+    (page.to_i - 1) * 10
+  end
+
+  def stars
+    h.content_tag(:div, nil) do
+      round_average.times do
+        h.concat(star)
+      end
+      (10 - round_average).times do
+        h.concat(empty_star)
+      end
+    end
+  end
+
+  private
+
+  def star
+    h.content_tag(:span, nil, class: 'glyphicon glyphicon-star')
+  end
+
+  def empty_star
+    h.content_tag(:span, nil, class: 'glyphicon glyphicon-star-empty')
+  end
+
+  def round_average
+    average.nil? ? 0 : average.round
+  end
+
+  def average
+    object.rates.average(:number)
+  end
+
+end
